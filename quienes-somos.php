6077cf28e60105ad9c33dc79f97b211aacf27c09
frndguerrero <fermaster.develop@gmail.com>
2025-12-22 22:08:23 +0100

    fix: agregué clase main somos ya justes de gradient para que no toques los otros carousel

--- quienes-somos.php
+++ quienes-somos.php
@@ -1,9 +1,9 @@
-<?php 
- $title = "Nosotros | Mercería Larraz";
- $description = 'Página de Productos de Mercería';
-require '_partials/header.php';?>
-<?php require '_partials/nav.php';?>
-  <main>
+<?php
+$title = "Nosotros | Mercería Larraz";
+$description = 'Página de Productos de Mercería';
+require '_partials/header.php'; ?>
+<?php require '_partials/nav.php'; ?>
+<main class="main-somos">
     <div id="myCarousel" class="carousel slide mb-6" data-bs-ride="carousel">
       <div class="carousel-indicators">
         <button
@@ -25,7 +25,7 @@ require '_partials/header.php';?>
           aria-label="Slide 3"></button>
       </div>
       <div class="carousel-inner">
-        <div class="carousel-item somos active">
+        <div class="carousel-item active">
           <svg
             aria-hidden="true"
             class="bd-placeholder-img"
@@ -45,13 +45,10 @@ require '_partials/header.php';?>
               <p class="opacity-75">
                 Nuestra historia
               </p>
-              <p>
-                <a class="btn-cta btn-lg" href="#nuestrahistoria">Ver más</a>
-              </p>
             </div>
           </div>
         </div>
-        <div class="carousel-item somos">
+        <div class="carousel-item">
           <svg
             aria-hidden="true"
             class="bd-placeholder-img"
@@ -71,11 +68,10 @@ require '_partials/header.php';?>
               <p>
                 Nuestra historia
               </p>
-              <p><a class="btn-cta btn-lg" href="#nuestrahistoria">Ver más</a></p>
             </div>
           </div>
         </div>
-        <div class="carousel-item somos">
+        <div class="carousel-item">
           <svg
             aria-hidden="true"
             class="bd-placeholder-img"
@@ -84,7 +80,7 @@ require '_partials/header.php';?>
             width="100%"
             xmlns="http://www.w3.org/2000/svg">
             <image
-              href="assets/img/cat-merceria.jpg"
+              href="assets/img/cat-textil.jpg"
               width="100%"
               height="100%"
               preserveAspectRatio="xMidYMid slice" />
@@ -94,9 +90,6 @@ require '_partials/header.php';?>
               <h1>Quienes Somos</h1>
               <p>
                 Nuestra historia
-              </p>
-              <p>
-                <a class="btn-cta btn-lg" href="#nuestrahistoria">Ver más</a>
               </p>
             </div>
           </div>
@@ -131,20 +124,18 @@ require '_partials/header.php';?>
             <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1z" />
           </svg>
 
-          <h2 class="fw-normal">Misión</h2>
-          <p>
-            Larraz tiene como misión ofrecer a sus clientes una experiencia única en el mundo de las manualidades, la mercería y las labores textiles, combinando tradición, creatividad e innovación. Desde el corazón de Zaragoza, buscamos inspirar la expresión artística y artesanal de personas de todas las edades mediante una atención personalizada, una cuidada selección de productos de calidad —nacionales e importados— y un entorno que une la elegancia clásica con las últimas tendencias del sector.
-          </p>
+          <h2 class="fw-bold title-red">Misión</h2>
+          <p class="lead">
+            Larraz tiene como misión ofrecer a sus clientes una experiencia única en el mundo de las manualidades, la mercería y las labores textiles, combinando tradición, creatividad e innovación. Desde el corazón de Zaragoza.
         </div>
         <!-- /.col-lg-4 -->
         <div class="col-lg-6 text-center">
-          <svg xmlns="http://www.w3.org/2000/svg" width="90" height="90" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
-            <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z" />
-            <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0" />
+          <svg xmlns="http://www.w3.org/2000/svg" width="90" height="90" fill="currentColor" class="bi bi-bar-chart" viewBox="0 0 16 16">
+            <path d="M4 11H2v3h2zm5-4H7v7h2zm5-5v12h-2V2zm-2-1a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM6 7a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1zm-5 4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1z" />
           </svg>
-          <h2 class="fw-normal">Visión</h2>
-          <p>
-            Ser reconocida como la tienda de referencia en España en manualidades textiles, mercería y artes creativas, destacando por nuestra capacidad de adaptación, nuestra trayectoria centenaria y nuestro compromiso con la innovación constante. Aspiramos a mantener viva la tradición artesanal mientras incorporamos nuevas tecnologías, productos y experiencias que impulsen la creatividad y consoliden a Larraz como un símbolo de calidad, inspiración y modernidad.
+          <h2 class="fw-bold title-red">Visión</h2>
+          <p class="lead">
+            Ser reconocida como la tienda de referencia en España en manualidades textiles, mercería y artes creativas, destacando por nuestra capacidad de adaptación, nuestra trayectoria centenaria.
           </p>
         </div>
       </div>
@@ -152,8 +143,8 @@ require '_partials/header.php';?>
       <!-- START THE FEATURETTES -->
       <hr class="featurette-divider" />
       <div class="row featurette">
-        <div class="col-md-7">
-          <h2 class="featurette-heading fw-normal lh-1">
+        <div class="col-md-7 pe-5">
+          <h2 class="featurette-heading fw-bold lh-1 title-red">
             Nuestra Historia
           </h2>
           <p class="lead">
@@ -163,26 +154,13 @@ require '_partials/header.php';?>
           </p>
         </div>
         <div class="col-md-5">
-          <svg
-            aria-label="Placeholder: 500x500"
-            class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
-            height="500"
-            preserveAspectRatio="xMidYMid slice"
-            role="img"
-            width="500"
-            xmlns="http://www.w3.org/2000/svg">
-            <image
-              href="assets/img/historia.jpg"
-              width="100%"
-              height="100%"
-              preserveAspectRatio="xMidYMid slice" />
-          </svg>
+          <img src="assets/img/historia.jpg" alt="" class="img1 pt-2" srcset="">
         </div>
       </div>
       <hr class="featurette-divider" />
       <div class="row featurette">
-        <div class="col-md-7 order-md-2">
-          <h2 class="featurette-heading fw-normal lh-1">
+        <div class="col-md-7 order-md-2 ps-5">
+          <h2 class="featurette-heading fw-bold lh-1 title-red">
             La Tienda
           </h2>
           <p class="lead">
@@ -192,49 +170,22 @@ require '_partials/header.php';?>
           </p>
         </div>
         <div class="col-md-5 order-md-1">
-          <svg
-            aria-label="Placeholder: 500x500"
-            class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
-            height="500"
-            preserveAspectRatio="xMidYMid slice"
-            role="img"
-            width="500"
-            xmlns="http://www.w3.org/2000/svg">
-            <image
-              href="assets/img/cajaentrada.jpg"
-              width="100%"
-              height="100%"
-              fill="var(--bs-secondary-bg)">
-              <text x="50%" y="50%" fill="var(--bs-secondary-color)" dy=".3em">
-                500x500
-              </text>
-          </svg>
+          <img src="assets/img/cajaentrada.jpg" alt="" class="img1" srcset="">
         </div>
       </div>
       <hr class="featurette-divider" />
       <div class="row featurette">
-        <div class="col-md-7">
-          <h2 class="featurette-heading fw-normal lh-1">
+        <div class="col-md-7 pe-5">
+          <h2 class="featurette-heading fw-bold lh-1 title-red">
             Confianza, calidad y seguridad
           </h2>
-          <p><strong>- Confianza:</strong> servimos a miles de clientes que confían en nosotros. Organismos oficiales como la Casa Real, ministerios, gobiernos autonómicos y ayuntamientos, compañías multinacionales y marcas reconocidas, clubes y federaciones deportivas, Ejército, Guardia Civil y otros cuerpos de seguridad, cofradías, partidos políticos y sindicatos, etc.</p>
-          <p>También distribuyen nuestros productos empresas como agencias de publicidad, tiendas de banderas y artículos personalizados, etc.<br /><strong>- Calidad:</strong> el conocimiento acumulado durante nuestra larga existencia nos permite ofrecer una amplia variedad de productos con la máxima calidad.</p>
-          <p>Algunos de nuestros productos son los de mayor calidad en España.<br /><strong>- Seguridad:</strong> satisfacemos las necesidades de nuestros clientes y cumplimos las fechas de entrega demandadas.</p>
+          <p class="lead"><strong>- Confianza:</strong> servimos a miles de clientes que confían en nosotros. Organismos oficiales como la Casa Real, ministerios, gobiernos autonómicos y ayuntamientos, compañías multinacionales y marcas reconocidas, clubes y federaciones deportivas, Ejército, Guardia Civil y otros cuerpos de seguridad, cofradías, partidos políticos y sindicatos, etc.</p>
+          <p class="lead">También distribuyen nuestros productos empresas como agencias de publicidad, tiendas de banderas y artículos personalizados, etc.<br /><strong>- Calidad:</strong> el conocimiento acumulado durante nuestra larga existencia nos permite ofrecer una amplia variedad de productos con la máxima calidad.</p>
+          <p class="lead">Algunos de nuestros productos son los de mayor calidad en España.<br /><strong>- Seguridad:</strong> satisfacemos las necesidades de nuestros clientes y cumplimos las fechas de entrega demandadas.</p>
         </div>
-        <div class="col-md-5">
-          <svg
-            aria-label="Placeholder: 500x500"
-            class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
-            height="500"
-            preserveAspectRatio="xMidYMid slice"
-            role="img"
-            width="500"
-            xmlns="http://www.w3.org/2000/svg">
-            <image
-              href="assets/img/manualidades.png"
-              width="100%"
-              height="100%"
-              fill="var(--bs-secondary-bg)">
+        <div class="col-md-5 pe-5
+        ">
+          <img src="assets/img/manualidades.png" alt="" class="img1 pt-2" srcset="">
           </svg>
         </div>
       </div>
